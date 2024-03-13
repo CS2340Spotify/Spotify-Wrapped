56d3dc2a2ac271821ee3fd52d8df47637fd9e2a9
vishva <koladiyavishva@gmail.com>
2024-03-13 12:55:11 -0400

    Added an option to update email

--- app/src/main/java/com/example/spotify_wrapped/UpdateAccountViewModel.java
+++ app/src/main/java/com/example/spotify_wrapped/UpdateAccountViewModel.java
@@ -1,7 +1,32 @@
 package com.example.spotify_wrapped;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
 public class UpdateAccountViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    private final MutableLiveData<UpdateStatus> updateStatus = new MutableLiveData<>();
+    public LiveData<UpdateStatus> getUpdateStatus() {
+        return updateStatus;
+    }
+    public void updateAccount(User user) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new FormBody.Builder()
+                .add("name", user.getName())
+                .add("username", user.getUsername())
+                .add("password", user.getPassword())
+                .add("email", user.getEmail())
+                .build();
+    }
+}
+
+enum UpdateStatus {
+    SUCCESS,
+    FAILURE
 }
