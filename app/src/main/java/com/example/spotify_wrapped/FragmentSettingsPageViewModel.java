ac8c25b2b6abf05460ef2592a9b0ea25d471c7cb
DevikaPapal <93013221+DevikaPapal@users.noreply.github.com>
2024-03-13 14:01:11 -0400

    created settings page

---
+++ app/src/main/java/com/example/spotify_wrapped/FragmentSettingsPageViewModel.java
@@ -0,0 +1,7 @@
+package com.example.spotify_wrapped;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragmentSettingsPageViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/spotify_wrapped/fragment_settings_page.java
@@ -0,0 +1,36 @@
+package com.example.spotify_wrapped;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_settings_page extends Fragment {
+
+    private FragmentSettingsPageViewModel mViewModel;
+
+    public static fragment_settings_page newInstance() {
+        return new fragment_settings_page();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings_page, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FragmentSettingsPageViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
