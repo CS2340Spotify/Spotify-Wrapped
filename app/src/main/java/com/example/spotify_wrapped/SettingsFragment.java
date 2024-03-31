17795ca1e5c9d4ea733a2b4a3b27e16b9cb691a6
adhishreekadam <kadam.adhishree@gmail.com>
2024-03-30 23:52:01 -0400

    connected settings and update page

--- app/src/main/java/com/example/spotify_wrapped/SettingsFragment.java
+++ app/src/main/java/com/example/spotify_wrapped/SettingsFragment.java
@@ -1,7 +1,9 @@
 package com.example.spotify_wrapped;
 
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -25,12 +27,30 @@ public class SettingsFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings_page, container, false);
     }
-
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(SettingsFragmentViewModel.class);
-        // TODO: Use the ViewModel
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        View updateProfileBtn = view.findViewById(R.id.update_profile);
+
+        // on click listener for the button
+        updateProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment updateAccountFragment = new UpdateAccountFragment();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, updateAccountFragment);
+                transaction.commit();
+            }
+        });
     }
+
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//        mViewModel = new ViewModelProvider(this).get(SettingsFragmentViewModel.class);
+//        // TODO: Use the ViewModel
+//    }
 
 }
