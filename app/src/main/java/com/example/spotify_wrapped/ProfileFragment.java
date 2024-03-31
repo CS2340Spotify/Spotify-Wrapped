4c34fad8d83b2d047ec707768ac32fe1f0076c74
DevikaPapal <93013221+DevikaPapal@users.noreply.github.com>
2024-03-31 19:24:43 -0400

    connected settings page to profile page

--- app/src/main/java/com/example/spotify_wrapped/ProfileFragment.java
+++ app/src/main/java/com/example/spotify_wrapped/ProfileFragment.java
@@ -1,5 +1,51 @@
 package com.example.spotify_wrapped;
 
-public class ProfileFragment {
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+public class ProfileFragment extends Fragment {
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings_page, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton settingsButton = view.findViewById(R.id.settings_button);
+
+        // on click listener for the Settings button on profile page
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment settingsFragment = new SettingsFragment();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, settingsFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+    }
+
 
 }
+
+
